ebbc4de44242f268d6814fc942ed9547d708c59c
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-03-21 12:33:21 +0300

    patch:ui

---
+++ js/index.js
@@ -0,0 +1,33 @@
+add_order_qnty.addEventListener("click",addOrderQuantity);
+
+function addOrderQuantity(e){
+    e.preventDefault();
+    quantityVar = quantityVar + 1;
+    quantity_ordered.innerHTML = quantityVar;
+
+    //set price according to quantity
+    let number_of_items_ordered = parseInt(quantity_ordered);
+    let item_price = parseInt(total_price_content);
+    total_price.innerHTML = currentPrice * number_of_items_ordered;
+}
+
+remove_order_qnty.addEventListener("click", removeOrderQnty)
+function removeOrderQnty(e){
+    e.preventDefault();
+    if(quantityVar<2){
+        alert("Can not order 0 items")
+        return;
+    }
+    quantityVar = quantityVar - 1;
+    quantity_ordered.innerHTML = quantityVar;
+
+    //set price according to quantity
+    let number_of_items_ordered = parseInt(quantity_ordered);
+    let item_price = parseInt(total_price_content);
+    total_price.innerHTML =  currentPrice * number_of_items_ordered;
+}
+
+add_order.addEventListener("click", (e)=>{
+    $(".reset").show();
+    $(".order-summary").hide();
+});
--- js/script.js
+++ js/script.js
@@ -60,10 +60,8 @@ function placeOrder (e){
                         let topping_desc =document.querySelector(".topping-desc").innerHTML;
                         let selectedTopping = ` ${topping_desc} ${j.nextElementSibling.innerHTML},`;
                         topping_desc_para.innerHTML =selectedTopping;
-                        console.log(topping_desc);
 
                         quantity_ordered.innerHTML = quantityVar;
-                        console.log(quantityVar);
                         
                     }
                     
@@ -72,40 +70,7 @@ function placeOrder (e){
         }
     }
     $(".reset").hide();
-    console.log(currentPrice);
     
     }
+ 
 
-console.log(currentPrice)    
-add_order_qnty.addEventListener("click",addOrderQuantity);
-
-function addOrderQuantity(e){
-    e.preventDefault();
-    quantityVar = quantityVar + 1;
-    quantity_ordered.innerHTML = quantityVar;
-
-    //set price according to quantity
-    let number_of_items_ordered = parseInt(quantity_ordered);
-    let item_price = parseInt(total_price_content);
-    total_price.innerHTML = currentPrice * number_of_items_ordered;
-}
-remove_order_qnty.addEventListener("click", removeOrderQnty)
-function removeOrderQnty(e){
-    e.preventDefault();
-    if(quantityVar<2){
-        alert("Can not order 0 items")
-        return;
-    }
-    quantityVar = quantityVar - 1;
-    quantity_ordered.innerHTML = quantityVar;
-
-    //set price according to quantity
-    let number_of_items_ordered = parseInt(quantity_ordered);
-    let item_price = parseInt(total_price_content);
-    total_price.innerHTML =  currentPrice * number_of_items_ordered;
-}
-
-add_order.addEventListener("click", (e)=>{
-    $(".reset").show();
-    $(".order-summary").hide();
-});
